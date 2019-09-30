8af46240e1310dc45b2e28b181b95c0cfb0ee891
A.Farahani <arashfarahani@hotmail.com>
2019-09-30 16:46:08 +0330

    Bridge Patterns has been added

---
+++ src/main/java/com/dp/bridge/StackImpl.java
@@ -0,0 +1,9 @@
+package com.dp.bridge;
+
+public interface StackImpl {
+    void push(int i);
+    int pop();
+    int top();
+    boolean isEmpty();
+    boolean isFull();
+}
---
+++ test/java/com/dp/adapter/AdapterTest.java
@@ -0,0 +1,26 @@
+package com.dp.adapter;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class AdapterTest {
+    @Mock private Line line;
+    @Mock private Rectangle rectangle;
+
+    @InjectMocks private LineAdapter lineAdapter;
+    @InjectMocks private RectangleAdapter rectangleAdapter;
+
+    @Test
+    public void drawLine() {
+        this.lineAdapter.draw(1, 10, 1, 10);
+    }
+
+    @Test
+    public void drawRectangle() {
+        this.rectangleAdapter.draw(1, 10, 5, 15);
+    }
+}
