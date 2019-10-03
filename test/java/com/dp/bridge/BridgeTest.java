3898428b82c30c7eba5333ddc57a99f760faeb84
Arash <arashfarahani@hotmail.com>
2019-10-03 06:33:56 +0330

    bridge test has been separated

--- test/java/com/dp/bridge/BridgeTest.java
+++ test/java/com/dp/bridge/BridgeTest.java
@@ -2,74 +2,15 @@ package com.dp.bridge;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
 
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        StackArrayTest.class,
+        StackFIFOTest.class,
+        StackHanoiTest.class,
+        StackListTest.class
+})
 public class BridgeTest {
-    @Before
-    public void separateResult() {
-        System.out.println("***********************************************************************");
-    }
-
-    @Test
-    public void generateStackArray() {
-        Stack stack = new Stack("array");
-
-        for (int i = 0; i < 10; i++)
-            stack.push(i);
-
-        int expectedValue = 9;
-
-        while (!stack.isEmpty()) {
-            int value = stack.pop();
-            System.out.println(value);
-            assert value == expectedValue--;
-        }
-    }
-
-    @Test
-    public void generateStackList() {
-        Stack stack = new Stack("list");
-
-        for (int i = 0; i < 10; i++)
-            stack.push(i);
-
-        int expectedValue = 9;
-
-        while (!stack.isEmpty()) {
-            int value = stack.pop();
-            System.out.println(value);
-            assert value == expectedValue--;
-        }
-    }
-
-    @Test
-    public void generateStackFIFO() {
-        Stack stack = new StackFIFO();
-
-        for (int i = 0; i < 10; i++)
-            stack.push(i);
-
-        int expectedValue = 0;
-
-        while (!stack.isEmpty()) {
-            int value = stack.pop();
-            System.out.println(value);
-            assert value == expectedValue++;
-        }
-    }
-
-    @Test
-    public void generateStackHanoi() {
-        Stack stack = new StackHanoi();
-
-        for (int i = 9; i >= 0; i--)
-            stack.push(i);
-
-        int expectedValue = 0;
-
-        while (!stack.isEmpty()) {
-            int value = stack.pop();
-            System.out.println(value);
-            assert value == expectedValue++;
-        }
-    }
 }
---
+++ test/java/com/dp/bridge/StackHanoiTest.java
@@ -0,0 +1,28 @@
+package com.dp.bridge;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class StackHanoiTest {
+    private Stack stack;
+
+    @Before
+    public void initialize() {
+        System.out.println("***********************************************************************");
+        this.stack = new StackHanoi();
+
+        for (int i = 9; i >= 0; i--)
+            this.stack.push(i);
+    }
+
+    @Test
+    public void generate() {
+        int expectedValue = 0;
+
+        while (!this.stack.isEmpty()) {
+            int value = this.stack.pop();
+            System.out.println(value);
+            assert value == expectedValue++;
+        }
+    }
+}
